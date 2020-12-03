9b21f36a2528c613141a730fb35adeb16b2f3e5e
daniyal <daniyalnaeem5@gmail.com>
2020-12-03 08:53:43 +0500

    controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,14 @@
 import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+
+  return new Promise(
+    Product.find({})
+  ) 
 
   res.json(products)
 })
